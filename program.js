cf8eebe74e3a5d66f55f3ec89057ba29da6b8d3e
Jackson <jacksontlockard@gmail.com>
2018-04-14 11:25:58 -0400

    refactored program.js into functions

--- program.js
+++ program.js
@@ -1,9 +1,17 @@
-var riddle = "What do you call a 100-year-old ant?";
-var riddleAnswer = "An antique";
+const riddle = "What do you call a 100-year-old ant?";
+const riddleAnswer = "An antique";
 var userAnswer = prompt(riddle);
 
 if (riddleAnswer.toLowerCase() == userAnswer.toLowerCase()) {
-  alert("You're right!");
+  correct();
 } else {
-  alert("You are wrong. Ha, ha! The correct answer is: " + riddleAnswer + "!");
+  incorrect(riddleAnswer);
+}
+
+function correct() {
+  alert("You're right!");
+}
+
+function incorrect(answer) {
+  alert("You are wrong. Ha, ha! The correct answer is: " + answer + "!");
 }
